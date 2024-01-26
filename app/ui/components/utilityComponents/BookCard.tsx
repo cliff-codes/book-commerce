0997a0bbe05d0e1ec4b9a42dc248742b3145fe9a
cliff-codes <108398436+cliff-codes@users.noreply.github.com>
2024-01-26 12:09:07 -0800

    BookCard page link route implemented

--- app/ui/components/utilityComponents/BookCard.tsx
+++ app/ui/components/utilityComponents/BookCard.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link'
 import React from 'react'
+
 
 interface BookData {
   title: string,
@@ -8,25 +10,27 @@ interface BookData {
 
 const BookCard = ({title, price, coverImg} : BookData) => {
   return (
-    <div className='w-full'>
-      <div className="card w-full min-w-60  h-auto bg-base-100 shadow-xl hover:shadow-2xl transition-all .1 ease-linear">
-        <figure className="px-3 pt-5 box-border ">
-          <img  width={100} src={coverImg} className='w-32' />
-        </figure>
-        <div className="card-body pl-5">
-          <h2 className="text-lg">{title}</h2>
-          
-          {/* card price and action button to buy */}
-          <div className='flex w-full  place-items-center justify-between'>
-            <div className='font-semibold'>${price}</div>
+    <Link href={'/bookId'}>
+      <div className='w-full'>
+        <div className="card w-full min-w-60  h-auto bg-base-100 shadow-xl hover:shadow-2xl transition-all .1 ease-linear">
+          <figure className="px-3 pt-5 box-border ">
+            <img  width={100} src={coverImg} className='w-32' />
+          </figure>
+          <div className="card-body pl-5">
+            <h2 className="text-lg">{title}</h2>
+            
+            {/* card price and action button to buy */}
+            <div className='flex w-full  place-items-center justify-between'>
+              <div className='font-semibold'>${price}</div>
 
-            <div className="card-actions">
-              <button className="btn btn-primary btn-sm custom-xs:btn-md bg-orange-500 border-none hover:bg-orange-600 text-slate-50">Buy Now</button>
+              <div className="card-actions">
+                <button className="btn btn-primary btn-sm custom-xs:btn-md bg-orange-500 border-none hover:bg-orange-600 text-slate-50">Buy Now</button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
