ca644e25aba9f4e1d9be37349cb7056050dd5759
Clifford <simplecodes2580@gmail.com>
2024-03-11 05:14:01 +0000

    logo display config

--- app/ui/components/navs/HomeNav.tsx
+++ app/ui/components/navs/HomeNav.tsx
@@ -25,7 +25,7 @@ const HomeNav = () => {
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                 </label>
               </div> 
-              <div className="flex-1 px-1 mx-1">
+              <div className="flex-1 px-1 mx-1 hidden custom-xs:flex">
                 <Logo/> 
               </div>
             </div>
